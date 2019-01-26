d73b50c7fe5e691172d5c5459060eaffa1263c8a
Fernand <fernand.veyrier@epitech.eu>
2019-01-26 21:51:41 +0100

    Fix block transitions not starting on the right position

--- GGJ2019/Assets/Scripts/Blocks/ABlock.cs
+++ GGJ2019/Assets/Scripts/Blocks/ABlock.cs
@@ -10,5 +10,10 @@ namespace Assets.Scripts.Blocks
         public abstract float GetHeight();
 
         public abstract float GetWidth();
+
+        public virtual void SetBlockPosition(Vector3 pos)
+        {
+            transform.localPosition = pos;
+        }
     }
 }
