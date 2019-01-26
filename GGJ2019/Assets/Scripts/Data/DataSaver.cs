e5d761e93a8f02d2c47daf44353153ac7f42838b
Fernand <fernand.veyrier@epitech.eu>
2019-01-26 14:15:18 +0100

    Added ScoreHandler & DataSaving

---
+++ GGJ2019/Assets/Scripts/Data/DataSaver.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+namespace Assets.Scripts.Data
+{
+    /// <summary>
+    /// Handles the data persistence.
+    /// </summary>
+    public static class DataSaver
+    {
+        private const string savePath = "/save.dat";
+
+        private static Dictionary<string, object> _data = new Dictionary<string, object>();
+
+        static DataSaver()
+        {
+            LoadData();
+        }
+
+        private static void LoadData()
+        {
+            if (File.Exists(Application.persistentDataPath + savePath))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                FileStream file = File.Open(Application.persistentDataPath + savePath, FileMode.Open);
+                if (file.Length <= 0)
+                {
+                    file.Close();
+                    SaveData();
+                }
+                else
+                {
+                    _data = (Dictionary<string, object>)bf.Deserialize(file);
+                }
+
+                file.Close();
+                file.Dispose();
+            }
+            else
+            {
+                File.Create(Application.persistentDataPath + savePath);
+                SaveData();
+            }
+        }
+
+        public static void SaveData()
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Create(Application.persistentDataPath + savePath);
+            bf.Serialize(file, _data);
+            file.Close();
+            file.Dispose();
+        }
+
+        public static void SetValue<T>(string key, T value)
+        {
+            if (_data.ContainsKey(key))
+                _data[key] = value;
+            else
+                _data.Add(key, value);
+        }
+
+        public static T GetValue<T>(string key)
+        {
+            if (!_data.ContainsKey(key))
+            {
+                _data.Add(key, default(T));
+            }
+            return (T)_data[key];
+        }
+    }
+}
---
+++ GGJ2019/Assets/Scripts/Data/ScoreHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Data
+{
+    [Serializable]
+    public class ScoreHandler
+    {
+        private float _scoreSinceLastCombo;
+        private float _comboMultiplier = 1;
+
+        [SerializeField]
+        private AnimationCurve _comboCurve;
+
+        public float Score { get; private set; }
+
+        public float ComboMultiplier
+        {
+            get => _comboMultiplier;
+
+            private set
+            {
+                if (_comboMultiplier != value)
+                    OnComboChange?.Invoke(this, new ComboEvent { PreviousCombo = _comboMultiplier, CurrentCombo = value });
+                _comboMultiplier = value;
+            }
+        }
+
+        public event EventHandler<ComboEvent> OnComboChange;
+
+        public void AddPoints(float amount)
+        {
+            if (amount <= 0)
+            {
+                _scoreSinceLastCombo = 0;
+            }
+            ComboMultiplier = _comboCurve.Evaluate(_scoreSinceLastCombo);
+            Score += amount * ComboMultiplier;
+            _scoreSinceLastCombo += amount * ComboMultiplier;
+        }
+    }
+
+    public class ComboEvent : EventArgs
+    {
+        public float PreviousCombo;
+        public float CurrentCombo;
+    }
+}
