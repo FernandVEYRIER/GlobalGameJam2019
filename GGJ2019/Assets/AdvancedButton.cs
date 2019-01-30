6d47b0006f867963500cca3bae11c872ea99ad3e
Nicolas Constanty <nicolas.constanty@epitech.eu>
2019-01-30 17:43:21 +0100

    Fix Navigation Menu Pause

---
+++ GGJ2019/Assets/AdvancedButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class AdvancedButton : Button
+{
+    public override void Select()
+    {
+        if ((UnityEngine.Object)EventSystem.current == (UnityEngine.Object)null || EventSystem.current.alreadySelecting)
+            return;
+        EventSystem.current.SetSelectedGameObject(this.gameObject);
+        UpdateSelectionState(new BaseEventData(EventSystem.current));
+    }
+}
